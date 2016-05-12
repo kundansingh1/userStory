19bce5ab011db5842d51843a5d09c481c22aa559
kundansingh1 <kundan.singh979@gmail.com>
2016-05-12 18:38:36 +0530

    Token Middleware added

--- app/routes/api.js
+++ app/routes/api.js
@@ -71,5 +71,27 @@ module.exports = function(app, express) {
     })
 
   }) // api post login
+
+  app.use(function(req, res, next) {
+    console.log('Somebody just came to our app');
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    // check token
+    if(token) {
+      jsonwebtoken.verify(token, secretKey, function(err, decoded) {
+        if(err) {
+          res.status(403).send({success: false, message: 'Failed to authenticate user'});
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      res.status(403).send({success: false, message: 'No token provided'})
+    }
+  });
+
+
+
   return api;
 }
