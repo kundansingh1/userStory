f9681c61bad947b8c3b6b787efeaf2518c9dbde5
MACMINI-Client <macmini-client@MACMINI-Clients-Mac-mini.local>
2016-05-12 18:11:50 +0530

    Initial Commit

---
+++ app/routes/api.js
@@ -0,0 +1,75 @@
+var User = require('../models/user');
+var config = require('../../config');
+var jsonwebtoken = require('jsonwebtoken');
+var secretKey = config.secretKey;
+
+function createToken(user) {
+  var token = jsonwebtoken.sign({
+    _id: user._id,
+    name: user.name,
+    username: user.username
+  }, secretKey, {
+    expiresIn: 1440
+  });
+
+  return token;
+} // function createToken
+
+
+module.exports = function(app, express) {
+  var api = express.Router();
+  api.post('/signup', function(req, res) {
+    var user = new User({
+      name : req.body.name,
+      username : req.body.username,
+      password : req.body.password
+    });
+
+    user.save(function(err) {
+      if(err) {
+        res.send(err);
+        return;
+      }
+
+      res.json({message: 'User has been created'});
+    });
+  }); // api post signup
+
+  api.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+      if(err) {
+        res.send(err);
+        return;
+      }
+
+      res.json({users});
+    })
+  }) // api get users
+
+  api.post('/login', function(req, res) {
+    User.findOne({
+      username: req.body.username
+    }).select('password').exec(function(err, user) {
+      if(err) throw err;
+
+      if(!user) {
+        res.send({message: "User doesn't exist"});
+      } else if(user) {
+        var validPassword = user.comparePassword(req.body.password);
+        if(!validPassword) {
+          res.send({message: "Invalid Password"})
+        } else if(validPassword) {
+          //// token
+          var token = createToken(user);
+          res.json({
+            success: true,
+            message: 'Sucessfully login!',
+            token: token
+          })
+        }
+      }
+    })
+
+  }) // api post login
+  return api;
+}
